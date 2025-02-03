40e79a7e80c290b58cdd8bbfada0ae48c0facb96
Alexandru Popa <alex9popa@yahoo.com>
2025-02-03 17:23:07 +0200

    Adjusted evaluation set to include more items

--- run_bot.py
+++ run_bot.py
@@ -4,6 +4,8 @@ import pickle
 import numpy as np
 import torch
 from openai import OpenAI
+import json
+import time
 
 from utils import SYSTEM_DESCRIPTION, Utils
 
@@ -19,10 +21,10 @@ class RoadAssistBot(Utils):
     def __init__(self, model_name, dataset, evaluation_dataset):
         super().__init__(model_name, dataset, evaluation_dataset)
         self.use_mps = torch.backends.mps.is_available()
-        self.device = "mps" if self.use_mps else "cpu"
+        self.device = "cpu"
         self.model.to(self.device)
 
-        self.embeddings_file = f"embeddings/text_{model_name}.pkl"
+        self.embeddings_file = f"embeddings_v1/text_{model_name}.pkl"
         self.embeddings = self._load_embeddings()
         self.texts = self._load_texts()
         self.conversation_history = [{"role": "system", "content": SYSTEM_DESCRIPTION}]
@@ -32,32 +34,28 @@ class RoadAssistBot(Utils):
 
     def _load_embeddings(self):
         logging.info(f"Loading embeddings from {self.embeddings_file}")
+    
         with open(self.embeddings_file, "rb") as f:
             embeddings = pickle.load(f)
-        
-        if isinstance(embeddings, list): embeddings = np.array(embeddings)   
-        embeddings = torch.from_numpy(embeddings)
-        
-        return embeddings.to(self.device)
+        if isinstance(embeddings, list):
+            embeddings = np.array(embeddings)
+        return embeddings
 
     def _compute_similarity(self, query_embeddings):
-        return self.embeddings @ query_embeddings
+        return np.dot(self.embeddings, query_embeddings)
     
     def _rank_all_paragraph_indices_by_similarity(self, query):
         query_embedding = self._compute_embeddings(query, self.device)
         similarities = self._compute_similarity(query_embedding)
-
-        sorted_indices = torch.argsort(similarities, descending=True).tolist()
-        logging.info(f"Sorted indeces {sorted_indices}")
+        sorted_indices = np.argsort(similarities)[::-1].tolist()
         return sorted_indices
 
     def _find_most_similar_paragraphs_indeces(self, query, top_n=5):
         sorted_indices = self._rank_all_paragraph_indices_by_similarity(query)
         top_n_sorted_indices = sorted_indices[:top_n]
-        logging.info(f"Top n sorted indeces {top_n_sorted_indices}")
         return top_n_sorted_indices
 
-    def _find_most_similar_paragraphs(self, query, top_n=5):
+    def _find_most_similar_paragraphs(self, query, top_n=10):
         top_n_sorted_indices = self._find_most_similar_paragraphs_indeces(query, top_n)
         most_similar_texts = [self.texts[i] for i in top_n_sorted_indices]
         return "\n\n".join(most_similar_texts)
@@ -98,7 +96,7 @@ class RoadAssistBot(Utils):
             logging.warning(f"Message generation failed. Error: {e}")
             chat_response = "System is not available. Try again later."
         return chat_response
-    
+
     def generate_response_without_context(self, query):
         user_prompt = self._create_prompt_without_context(query)
         self.conversation_history.append({"role": "user", "content": user_prompt})
@@ -113,35 +111,113 @@ class RoadAssistBot(Utils):
             chat_response = "System is not available. Try again later."
         return chat_response
 
-    def evaluate_retrieval_and_generation(self, top_k=5):
-        precision_scores = []
-        recall_scores = []
+    def evaluate_with_llm(self, query, gen_answer, actual_answer, actual_context):
+        system_prompt = """
+            Ești un evaluator expert în legislația rutieră din România.
+        """
+        user_prompt = f"""
+            Ai primit o întrebare, un răspuns generat de un chatbot, răspunsul corect de referință si paragraful
+            din legislatia rutiera pe care il poti folosi ca adevar.
+            
+            Evaluează răspunsul generat pe baza următoarelor criterii:
+            Corectitudine: Răspunsul conține informații corecte și conforme cu legislația rutieră din România?
+            Completitudine: Răspunsul oferă toate detaliile esențiale sau omite informații importante?
+            Claritate: Răspunsul este formulat clar și ușor de înțeles?
+            Relevanță: Răspunsul este relevant pentru întrebare sau conține informații inutile?
+            
+            Intrare:
+            Întrebare: {query}
+            Răspuns generat: {gen_answer}
+            Răspuns corect de referință: {actual_answer}
+            Context: {actual_context}
+
+            Atribuie un scor de la 0 la 10 pentru raspunsul generat.
+            Trebuie sa returnezi un singur obiect JSON care sa aiba doar cheia "score".
+            Nu folosi elemente de markdown!
+        """
+        try:
+            completion = self.client.chat.completions.create(
+                model="gpt-4o-mini-2024-07-18",
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt}
+                ]
+            )
+            llm_response = completion.choices[0].message
+            score = json.loads(llm_response.content).get("score", None)
+        except Exception as e:
+            logging.warning(f"Message generation failed. Error: {e}")
+            score = None
+        time.sleep(3)
+        return score
+
+    @staticmethod
+    def _compute_keywords_score(keywords, text):
+        text_words = set(text.lower().split())
+        keywords_set = set(map(str.lower, keywords))
+        matched_keywords = keywords_set.intersection(text_words)
+        return len(matched_keywords) / len(keywords_set) if keywords_set else 0.0
+
+    @staticmethod
+    def _compute_paragraph_acc(top_sorted_indices, par_index):
+        return 1 if par_index in top_sorted_indices else 0
+
+    @staticmethod
+    def _check_retrieved_page(paragraph, index_list, range_size=2):
+        page_paragraphs = list(range(paragraph - range_size, paragraph + range_size + 1))
+        return 1 if any(p in index_list for p in page_paragraphs) else 0
+
+    @staticmethod
+    def _add_stats(**score_lists):
+        averages = {}
+        for key, values in score_lists.items():
+            if values:
+                averages[f"avg_{key}"] = sum(v for v in values if v is not None) / len(values)
+            else:
+                averages[f"avg_{key}"] = None if key == "highest_positions" else 0
+        return averages
+
+    def evaluate_retrieval_and_generation(self, top_k=10):
+        retrieval_accuracy_scores = []
         highest_positions = []
         bleu_scores = []
         rouge_l_scores = []
+        keywords_scores = []
+        llm_eval_scores = []
+        page_acc_scores = []
 
         rouge_scorer_obj = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=True)
+        results = []
 
         for entry in self.evaluation_dataset:
             query = entry["question"]
             golden_answer = entry["answer"]
-            relevant_indices = entry["paragraphs"]
+            actual_paragraph_index = entry["paragraph"]
+            actual_paragraph_text = self.texts[actual_paragraph_index]
 
-            retrieved_indices = self._find_most_similar_paragraphs_indeces(query, top_k)
+            # retrieval evaluation
             all_sorted_indices = self._rank_all_paragraph_indices_by_similarity(query)
+            top_1_par_retrieved = self.texts[all_sorted_indices[0]]
 
-            retrieved_relevant = set(retrieved_indices).intersection(set(relevant_indices))
-            precision = len(retrieved_relevant) / top_k
-            recall = len(retrieved_relevant) / len(relevant_indices) if relevant_indices else 0
+            highest_position = all_sorted_indices.index(actual_paragraph_index)
+            highest_positions.append(highest_position)
 
-            highest_position = min(
-                (all_sorted_indices.index(ri) for ri in relevant_indices),
-                default=None,
-            )
+            retrieval_acc = self._compute_paragraph_acc(all_sorted_indices[:top_k], actual_paragraph_index)
+            retrieval_accuracy_scores.append(retrieval_acc)
 
+            page_acc = self._check_retrieved_page(actual_paragraph_index, all_sorted_indices[:top_k])
+            page_acc_scores.append(page_acc)
+
+            # answer generation evaluation
             generated_response = self.generate_response(query)
             generated_response_text = generated_response.content
 
+            keywords_score = self._compute_keywords_score(entry["keywords"], generated_response_text)
+            keywords_scores.append(keywords_score)
+
+            llm_score = self.evaluate_with_llm(query, generated_response_text, golden_answer, actual_paragraph_text)
+            llm_eval_scores.append(llm_score)
+
             smooth_fn = SmoothingFunction().method1
             bleu = sentence_bleu(
                 [golden_answer.split()], generated_response_text.split(), smoothing_function=smooth_fn
@@ -151,49 +227,57 @@ class RoadAssistBot(Utils):
             rouge_score = rouge_scorer_obj.score(golden_answer, generated_response_text)["rougeL"].fmeasure
             rouge_l_scores.append(rouge_score)
 
-            precision_scores.append(precision)
-            recall_scores.append(recall)
-            highest_positions.append(highest_position)
+            results.append({
+                "query": query,
+                "actual_answer": golden_answer,
+                "gen_answer": generated_response_text,
+                "first_par_retrieved": top_1_par_retrieved,
+                "ret_acc": retrieval_acc,
+                "page_acc": page_acc,
+                "highest_position": highest_position,
+                "keywords_score": keywords_score,
+                "llm_eval_score": llm_score,
+                "bleu_score": bleu,
+                "rouge_score" : rouge_score 
+            })
 
-        avg_precision = sum(precision_scores) / len(precision_scores) if precision_scores else 0
-        avg_recall = sum(recall_scores) / len(recall_scores) if recall_scores else 0
-        avg_highest_position = (
-            sum(pos for pos in highest_positions if pos is not None) / len(highest_positions)
-            if highest_positions else None
-        )
-        avg_bleu = sum(bleu_scores) / len(bleu_scores) if bleu_scores else 0
-        avg_rouge_l = sum(rouge_l_scores) / len(rouge_l_scores) if rouge_l_scores else 0
+        stats = self._add_stats(
+            ret_acc=retrieval_accuracy_scores,
+            page_acc=page_acc_scores,
+            highest_position=highest_positions,
+            keywords_score=keywords_scores,
+            llm_score=llm_eval_scores,
+            bleu_score=bleu_scores,
+            rouge_score=rouge_l_scores
+        ) | {"min_highest_pos": min(highest_positions), "max_highest_pos": max(highest_positions)}
+        results.insert(0, stats)
 
-        evaluation_results = {
-            "precision": avg_precision,
-            "recall": avg_recall,
-            "avg_highest_position": avg_highest_position,
-            "bleu": avg_bleu,
-            "rougeL": avg_rouge_l,
-            "highest_positions": highest_positions,
-        }
+        res_file = f"results/res_mpnet_top10.json"
+        Utils.write_json(res_file, results)
 
-        log_dir = f"logs/evaluation"
-        log_file = f"{log_dir}/{self.model_name}@{top_k}.log"
-        Utils.write_json(log_file, evaluation_results)
-
-        logging.info(f"Evaluation results saved to {log_file}")
-
-        return evaluation_results
-    
     def evaluate_generation_without_context(self):
         bleu_scores = []
         rouge_l_scores = []
-
+        keywords_scores = []
+        llm_eval_scores = []
+        results = []
         rouge_scorer_obj = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=True)
 
         for entry in self.evaluation_dataset:
             query = entry["question"]
             golden_answer = entry["answer"]
+            actual_paragraph_index = entry["paragraph"]
+            actual_paragraph_text = self.texts[actual_paragraph_index]
 
             generated_response = self.generate_response_without_context(query)
             generated_response_text = generated_response.content
+            
+            keywords_score = self._compute_keywords_score(entry["keywords"], generated_response_text)
+            keywords_scores.append(keywords_score)
 
+            llm_score = self.evaluate_with_llm(query, generated_response_text, golden_answer, actual_paragraph_text)
+            llm_eval_scores.append(llm_score)
+            
             smooth_fn = SmoothingFunction().method1
             bleu = sentence_bleu(
                 [golden_answer.split()], generated_response_text.split(), smoothing_function=smooth_fn
@@ -202,30 +286,32 @@ class RoadAssistBot(Utils):
 
             rouge_score = rouge_scorer_obj.score(golden_answer, generated_response_text)["rougeL"].fmeasure
             rouge_l_scores.append(rouge_score)
+            
+            results.append({
+                "query": query,
+                "actual_answer": golden_answer,
+                "gen_answer": generated_response_text,
+                "keywords_score": keywords_score,
+                "llm_eval_score": llm_score,
+                "bleu_score": bleu,
+                "rouge_score" : rouge_score 
+            })
 
-        avg_bleu = sum(bleu_scores) / len(bleu_scores) if bleu_scores else 0
-        avg_rouge_l = sum(rouge_l_scores) / len(rouge_l_scores) if rouge_l_scores else 0
-
-        evaluation_results = {
-            "bleu": avg_bleu,
-            "bleu_scores": bleu_scores,
-            "rougeL": avg_rouge_l,
-            "rougeL_scores": rouge_l_scores
-        }
-
-        log_dir = f"logs/evaluation"
-        log_file = f"{log_dir}/no_context.log"
-        Utils.write_json(log_file, evaluation_results)
-
-        logging.info(f"Evaluation results saved to {log_file}")
-
-        return evaluation_results
+        stats = self._add_stats(
+            keywords_score=keywords_scores,
+            llm_score=llm_eval_scores,
+            bleu_score=bleu_scores,
+            rouge_score=rouge_l_scores
+        )
+        results.insert(0, stats)
+        res_file = f"results/res_without_context.json"
+        Utils.write_json(res_file, results)
 
 
 if __name__ == "__main__":
-    bot = RoadAssistBot(
-        model_name="readerbench/RoBERT-base",
-        dataset="data/paragraphs.json",
+    bot = RoadAssistBot(        
+        model_name="paraphrase-multilingual-mpnet-base-v2",
+        dataset="data/paragraphs_v1.json",
         evaluation_dataset="data/benchmark.json"
     )
 
@@ -239,5 +325,3 @@ if __name__ == "__main__":
 
     # results = bot.evaluate_retrieval_and_generation(top_k=5)
     results = bot.evaluate_generation_without_context()
-    print("Evaluation Results:")
-    print(results)
